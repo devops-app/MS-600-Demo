89b3280ca7a11278e7ded2601892c2cf773aebee
Wilson Tsui <Wilson.Tchui@gmail.com>
2023-01-12 10:31:13 -0500

    MS-600-Demo

---
+++ Demo/learn-msteams-taskmodules/src/client/youTubePlayer1Tab/VideoSelectorTaskModule.tsx
@@ -0,0 +1,56 @@
+
+import * as React from "react";
+import { Provider, Flex, Text, Button, Header, Input } from "@fluentui/react-northstar";
+import { useState, useEffect } from "react";
+import { useTeams } from "msteams-react-base-component";
+import { app, dialog } from "@microsoft/teams-js";
+
+export const VideoSelectorTaskModule = () => {
+
+    const [{ inTeams, theme, context }] = useTeams();
+    const [entityId, setEntityId] = useState<string | undefined>();
+    const [youTubeVideoId, setYouTubeVideoId] = useState<string | undefined>("VlEH4vtaxp4");
+
+    const getQueryVariable = (variable: string): string | undefined => {
+        const query = window.location.search.substring(1);
+        const vars = query.split("&");
+        for (const varPairs of vars) {
+            const pair = varPairs.split("=");
+            if (decodeURIComponent(pair[0]) === variable) {
+                return decodeURIComponent(pair[1]);
+            }
+        }
+        return undefined;
+    };
+
+    useEffect(() => {
+        if (inTeams === true) {
+            app.notifySuccess();
+        } else {
+            setEntityId("Not in Microsoft Teams");
+        }
+    }, [inTeams]);
+
+    useEffect(() => {
+        if (context) {
+            setEntityId(context.page.id);
+            setYouTubeVideoId(getQueryVariable("vid"));
+        }
+    }, [context]);
+
+    const handleOnChanged = (event): void => { setYouTubeVideoId(event.target.value); };
+
+    const handleOnClick = (): void => { dialog.submit(youTubeVideoId, undefined); };
+
+    return (
+        <Provider theme={theme}>
+            <Flex column gap="gap.smaller">
+                <Text size="medium">
+                    Enter the ID of a YouTube video to show in the task module player.
+                </Text>
+                <Input value={youTubeVideoId} onChange={(e) => handleOnChanged(e)}></Input>
+                <Button content="Update" primary onClick={() => handleOnClick()}></Button>
+            </Flex>
+        </Provider>
+    );
+};
